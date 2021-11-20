0595eb226f3344c3c8b9e5d62e8c5a6f518725cf
wtwalker1 <wtwalker1@gmail.com>
2021-11-20 12:06:38 -0600

    add countdown timer

--- js/app.js
+++ js/app.js
@@ -3,3 +3,19 @@
 console.log('app.js is connected.');
 
 let greetingMessage = alert('Welcome to my clothing website!')
+
+const startingHours = 72;
+let time = startingHours * 24 * 60;
+
+const countdownEl = document.getElementById('countdown')
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const hours = Math.floor(time / 24)
+    let minutes = time % 60;
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = '${hours}: ${minutes}: ${seconds}';
+    time--;
+}
